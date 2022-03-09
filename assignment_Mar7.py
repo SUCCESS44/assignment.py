d6dc58a1eb2215308fa5aaa2ec871e5c4d76a640
SUCCESS44 <ononuju.okere@yahoo.com>
2022-03-09 13:14:11 -0500

    assignment_Mar7.py modified

--- assignment_Mar7.py
+++ assignment_Mar7.py
@@ -2,7 +2,7 @@
 import click
 
 @click.command() #creates a basic click application for you
-@click.option('--scripting', default ='bash', help='How do you want to call your script')
+@click.option('--scripting', default ='bash', help='what do you want to call your script')
 @click.option('--name', default ='shell', help="How is your script")
 def language(name, scripting):
     print(f"{scripting} {name}")
