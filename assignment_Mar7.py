b68978447f3bb13bba99e806830551b8fa338d31
SUCCESS44 <ononuju.okere@yahoo.com>
2022-03-09 13:06:08 -0500

    assignment_Mar7.py added

---
+++ assignment_Mar7.py
@@ -0,0 +1,11 @@
+#!/home/ojokere/Development/python_project/bin/python
+import click
+
+@click.command() #creates a basic click application for you
+@click.option('--scripting', default ='bash', help='How do you want to call your script')
+@click.option('--name', default ='shell', help="How is your script")
+def language(name, scripting):
+    print(f"{scripting} {name}")
+
+if __name__ == "__main__":
+    language()    
